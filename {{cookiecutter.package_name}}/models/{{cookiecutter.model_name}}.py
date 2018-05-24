bf4992339a0d2ff031f146a91829bc830e4e5338
Kapil Gupta <kpgupta98@gmail.com>
2018-05-24 11:28:36 +0530

    edit

--- {{cookiecutter.package_name}}/models/{{cookiecutter.model_name}}.py
+++ {{cookiecutter.package_name}}/models/{{cookiecutter.model_name}}.py
@@ -1,1 +1,10 @@
-{{cookiecutter.model_name}}.py
+#!/usr/bin/env python
+
+import torch
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self, config):
+        super(Model, self).__init__()
+        self.config = config
+        
