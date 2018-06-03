69c8747699703b64d0f2a266c9a555ea3bf96409
Kapil Gupta <kpgupta98@gmail.com>
2018-06-03 16:09:58 +0530

    new main function

--- {{cookiecutter.package_name}}/main.py
+++ {{cookiecutter.package_name}}/main.py
@@ -6,13 +6,17 @@ import argparse
 from argparse import RawTextHelpFormatter
 import time
 
+import matplotlib.pyplot as plt
 import numpy as np
 from functools import reduce
 import torch
 import torch.nn as nn
 from torch.optim import Adam
 import torch.distributed as dist
+from torch.backends import cudnn
 import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 from torch.autograd import Variable
 
 from dataloader import *
@@ -20,57 +24,8 @@ from models import *
 from utils import *
 
 best_prec1 = 0
+configure("logs/log-1")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-def count_parameters(model):
-    """Counts trainable(active) parameters of a model"""
-    total_params = sum(reduce( lambda a, b: a*b, x.size()) for x in model.parameters())
-    return total_params
-
-def save_checkpoint(state, is_best, loc, filename='checkpoint.pth.tar'):
-    file_loc = os.path.join(loc, filename)
-    torch.save(state, file_loc)
-    if is_best:
-        shutil.copyfile(file_loc, os.path.join(loc, 'model_best.pth.tar'))
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-def adjust_learning_rate(optimizer, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 30))
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
 
 def main(args):
     np.random.seed(args.seed)
@@ -83,53 +38,27 @@ def main(args):
         dist.init_process_group(backend=config.distributed['dist_backend'], init_method=config.distributed['dist_url'],\
                                 world_size=config.distributed['world_size'])
 
-    if args.pretrained:
-        model = Model(config, pretrained=True)
-    else:
-        model = Model(config)
+    # creating model instance
+    model = Model(config)
+    # plotting interactively
+    plt.ion()
 
-    if not args.distributed:
-        model = nn.DataParallel(model).to(device)
-    else:
+    if args.distributed:
         model.to(device)
         model = nn.parallel.DistributedDataParallel(model)
-
-    # define loss function (criterion) and optimizer
-    criterion = nn.CrossEntropyLoss().to(device)
-    optimizer = torch.optim.SGD(model.parameters(), config.hyperparameters['lr'],
-                                momentum=config.hyperparameters['momentum'],
-                                weight_decay=config.hyperparameters['weight_decay'])
-
-    # optionally resume from a checkpoint
-    if args.resume:
-        if os.path.isfile(args.checkpoint):
-            print("[!] Loading checkpoint '{}'".format(args.checkpoint))
-            checkpoint = torch.load(args.checkpoint)
-            args.start_epoch = checkpoint['epoch']
-            best_prec1 = checkpoint['best_prec1']
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            print("[+] Loaded checkpoint '{}' (epoch {})"
-                .format(args.checkpoint, checkpoint['epoch']))
-        else:
-            print("[-] No checkpoint found at '{}'".format(args.checkpoint))
-
-    cudnn.benchmark = True
+    elif args.gpu:
+        model = nn.DataParallel(model).to(device)
+    else: return
 
     # Data Loading
-    traindir = os.path.join(config.data['root'], 'train')
-    valdir = os.path.join(config.data['root'], 'val')
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+    train_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
+                                           download=True)
 
-    train_dataset = datasets.ImageFolder(
-        traindir,
-        transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ]))
+    test_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                              train=False,
+                                              transform=transforms.ToTensor())
 
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
@@ -141,39 +70,51 @@ def main(args):
         num_workers=config.data['workers'], pin_memory=config.data['pin_memory'], sampler=train_sampler)
 
     val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize,
-        ])),
-        batch_size=config.data['batch_size'], shuffle=config.data['shuffle'],
+        test_dataset, batch_size=config.data['batch_size'], shuffle=config.data['shuffle'],
         num_workers=config.data['workers'], pin_memory=config.data['pin_memory'])
 
-    if args.evaluate:
-        validate(val_loader, model, criterion)
-        return
+    # Training and Evaluation
+    trainer = Trainer('cnn', config, train_loader, model)
+    evaluator = Evaluator('cnn', config, val_loader, model)
 
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = torch.optim.SGD(model.parameters(), config.hyperparameters['lr'],
+                                momentum=config.hyperparameters['momentum'],
+                                weight_decay=config.hyperparameters['weight_decay'])
+
+    trainer.setCriterion(criterion)
+    trainer.setOptimizer(optimizer)
+    evaluator.setCriterion(criterion)
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        trainer.load_saved_checkpoint(checkpoint=None)
+
+    # Turn on benchmark if the input sizes don't vary
+    # It is used to find best way to run models on your machine
+    cudnn.benchmark = True
+
+    best_prec1 = 0
     for epoch in range(config.hyperparameters['num_epochs']):
         if args.distributed:
             train_sampler.set_epoch(epoch)
-        adjust_learning_rate(optimizer, epoch)
 
-        # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch)
+        trainer.adjust_learning_rate(epoch)
+        trainer.train(epoch)
 
-        # evaluate on validation set
-        prec1 = validate(val_loader, model, criterion)
+        prec1 = evaluator.evaluate(epoch)
 
         # remember best prec@1 and save checkpoint
         is_best = prec1 > best_prec1
         best_prec1 = max(prec1, best_prec1)
-        save_checkpoint({
+        trainer.save_checkpoint({
             'epoch': epoch+1,
             'state_dict': model.state_dict(),
             'best_prec1': best_prec1,
             'optimizer': optimizer.state_dict(),
-        }, is_best)
+        }, is_best, checkpoint=None)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Disentangling Variations', formatter_class=RawTextHelpFormatter)
