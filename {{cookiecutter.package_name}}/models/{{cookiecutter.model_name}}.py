b92f668b72f98d90662a76b3c2c19f53d3600b6a
Kapil Gupta <kpgupta98@gmail.com>
2018-06-03 16:09:32 +0530

    working model with some parameters

--- {{cookiecutter.package_name}}/models/{{cookiecutter.model_name}}.py
+++ {{cookiecutter.package_name}}/models/{{cookiecutter.model_name}}.py
@@ -7,6 +7,21 @@ class Model(nn.Module):
     def __init__(self, config):
         super(Model, self).__init__()
         self.config = config
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(7*7*32, self.config.data['num_classes'])
 
     def forward(self, x):
-        pass
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
