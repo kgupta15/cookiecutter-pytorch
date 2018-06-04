bfc3a1b2ced67604a84ed811ea9c89fb88330b1a
Kapil Gupta <kpgupta98@gmail.com>
2018-06-04 12:27:42 +0530

    normalization should be after non-linear op

--- {{cookiecutter.package_name}}/models/{{cookiecutter.model_name}}.py
+++ {{cookiecutter.package_name}}/models/{{cookiecutter.model_name}}.py
@@ -9,13 +9,13 @@ class Model(nn.Module):
         self.config = config
         self.layer1 = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(16),
             nn.ReLU(),
+            nn.BatchNorm2d(16),            
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
+            nn.BatchNorm2d(32),            
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.fc = nn.Linear(7*7*32, self.config.data['num_classes'])
 
